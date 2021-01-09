3ffebd1460538c63160d63687dd2aad01ef9a085
Guilherme Dezena <guilhermepereiradezena@gmail.com>
2021-01-09 16:08:24 -0300

    Cria projeto loteria

---
+++ Loteria/Loteria/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Gabriel on 09/01/21.
+//  Copyright © 2021 Guilherme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
